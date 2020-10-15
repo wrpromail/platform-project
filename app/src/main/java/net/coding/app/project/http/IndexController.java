5628ed9f4b9352a1ac04f5719167e83a8b4b820c
liuguangbo <liuguangbo@coding.net>
2020-10-15 11:49:50 +0800

    fix bug

--- app/src/main/java/net/coding/app/project/http/IndexController.java
+++ app/src/main/java/net/coding/app/project/http/IndexController.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 public class IndexController {
     @GetMapping("/")
     public String findProjectResourcesList() {
-        log.error("sentry error rrrrrrrr!!!!");
         return "success";
     }
 }
