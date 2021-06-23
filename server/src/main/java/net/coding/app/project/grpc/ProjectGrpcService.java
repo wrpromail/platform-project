2c7f5e9963acbf2b8dbf7ad84cbfa005e46826dd
liheng <liheng@coding.net>
2021-06-23 15:36:37 +0800

    add RPC Project #8490

--- server/src/main/java/net/coding/app/project/grpc/ProjectGrpcService.java
+++ server/src/main/java/net/coding/app/project/grpc/ProjectGrpcService.java
@@ -2,9 +2,13 @@ package net.coding.app.project.grpc;
 
 
 import net.coding.common.util.BeanUtils;
+import net.coding.grpc.client.permission.AclServiceGrpcClient;
 import net.coding.lib.project.entity.Project;
+import net.coding.lib.project.enums.ProjectTemplateEnums;
 import net.coding.lib.project.exception.CoreException;
 import net.coding.lib.project.grpc.client.TeamGrpcClient;
+import net.coding.lib.project.grpc.client.UserGrpcClient;
+import net.coding.lib.project.parameter.ProjectCreateParameter;
 import net.coding.lib.project.service.ProjectService;
 
 import org.apache.commons.collections.CollectionUtils;
@@ -12,7 +16,9 @@ import org.apache.commons.lang3.StringUtils;
 import org.lognet.springboot.grpc.GRpcService;
 import org.springframework.util.ObjectUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import io.grpc.stub.StreamObserver;
@@ -23,7 +29,20 @@ import proto.common.CodeProto;
 import net.coding.proto.platform.project.ProjectProto;
 import net.coding.proto.platform.project.ProjectServiceGrpc;
 
+import proto.platform.permission.PermissionProto;
 import proto.platform.team.TeamProto;
+import proto.platform.user.UserProto;
+
+import static net.coding.lib.project.exception.CoreException.ExceptionType.PERMISSION_DENIED;
+import static net.coding.lib.project.exception.CoreException.ExceptionType.TEAM_NOT_EXIST;
+import static net.coding.lib.project.exception.CoreException.ExceptionType.USER_NOT_LOGIN;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static proto.common.CodeProto.Code.INTERNAL_ERROR;
+import static proto.common.CodeProto.Code.SUCCESS;
+import static proto.common.CodeProto.Code.NOT_FOUND;
+import static proto.platform.permission.PermissionProto.Action.Create;
+import static proto.platform.permission.PermissionProto.Action.Delete;
+import static proto.platform.permission.PermissionProto.Function.EnterpriseProject;
 
 
 @Slf4j
@@ -33,6 +52,111 @@ public class ProjectGrpcService extends ProjectServiceGrpc.ProjectServiceImplBas
 
     private final ProjectService projectService;
     private final TeamGrpcClient teamGrpcClient;
+    private final UserGrpcClient userGrpcClient;
+    private final AclServiceGrpcClient aclServiceGrpcClient;
+
+    @Override
+    public void createProject(
+            ProjectProto.CreateProjectRequest request,
+            StreamObserver<ProjectProto.CreateProjectResponse> responseObserver) {
+        try {
+            UserProto.User currentUser = userGrpcClient.getUserById(request.getUserId());
+            if (Objects.isNull(currentUser)) {
+                throw CoreException.of(USER_NOT_LOGIN);
+            }
+            //验证用户接口权限
+            boolean hasPermissionInEnterprise = aclServiceGrpcClient.hasPermissionInEnterprise(
+                    PermissionProto.Permission.newBuilder()
+                            .setFunction(EnterpriseProject)
+                            .setAction(Create)
+                            .build(),
+                    currentUser.getId(),
+                    currentUser.getTeamId()
+            );
+            if (!hasPermissionInEnterprise) {
+                throw CoreException.of(PERMISSION_DENIED);
+            }
+            Project project = projectService.createProject(ProjectCreateParameter.builder()
+                    .userId(currentUser.getId())
+                    .teamId(currentUser.getTeamId())
+                    .name(request.getName().replace(" ", "-"))
+                    .displayName(request.getDisplayName())
+                    .description(request.getDescription())
+                    .icon(EMPTY)
+                    .groupId(null)
+                    .projectTemplate(request.getProjectTemplate().name())
+                    .template(EMPTY)
+                    .functionModule(new ArrayList<>())
+                    .build());
+            createProjectResponse(responseObserver, SUCCESS, SUCCESS.name(), project);
+        } catch (CoreException e) {
+            log.error("RpcService createProject error CoreException ", e);
+            createProjectResponse(responseObserver, NOT_FOUND, e.getMsg(), null);
+        } catch (Exception e) {
+            log.error("rpcService createProject error Exception ", e);
+            createProjectResponse(responseObserver, INTERNAL_ERROR, e.getMessage(), null);
+        }
+    }
+
+    @Override
+    public void deleteProject(
+            ProjectProto.DeleteProjectRequest request,
+            StreamObserver<ProjectProto.DeleteProjectResponse> responseObserver) {
+        try {
+            UserProto.User currentUser = userGrpcClient.getUserById(request.getUserId());
+            if (Objects.isNull(currentUser)) {
+                throw CoreException.of(USER_NOT_LOGIN);
+            }
+            //验证用户接口权限
+            boolean hasPermissionInProject = aclServiceGrpcClient.hasPermissionInProject(
+                    PermissionProto.Permission.newBuilder()
+                            .setFunction(EnterpriseProject)
+                            .setAction(Delete)
+                            .build(),
+                    request.getProjectId(),
+                    currentUser.getGlobalKey(),
+                    currentUser.getId()
+            );
+            if (!hasPermissionInProject) {
+                throw CoreException.of(PERMISSION_DENIED);
+            }
+            projectService.delete(currentUser.getId(), currentUser.getTeamId(), request.getProjectId());
+            deleteProjectResponse(responseObserver, SUCCESS, SUCCESS.name());
+        } catch (CoreException e) {
+            log.error("RpcService deleteProject error CoreException ", e);
+            deleteProjectResponse(responseObserver, NOT_FOUND, e.getMsg());
+        } catch (Exception e) {
+            log.error("rpcService deleteProject error Exception ", e);
+            deleteProjectResponse(responseObserver, INTERNAL_ERROR, e.getMessage());
+        }
+    }
+
+    @Override
+    public void existProjectByName(
+            ProjectProto.ExistProjectByNameRequest request,
+            StreamObserver<ProjectProto.ExistProjectByNameResponse> responseObserver) {
+        try {
+            TeamProto.GetTeamResponse response = teamGrpcClient.getTeam(request.getTeamId());
+            if (Objects.isNull(response) || CodeProto.Code.SUCCESS != response.getCode()
+                    || ObjectUtils.isEmpty(response.getData())) {
+                throw CoreException.of(TEAM_NOT_EXIST);
+            }
+            projectService.validateCreateProjectParameter(ProjectCreateParameter.builder()
+                    .teamId(response.getData().getId())
+                    .name(request.getProjectName().replace(" ", "-"))
+                    .displayName(request.getProjectName())
+                    .projectTemplate(ProjectTemplateEnums.DEV_OPS.name())
+                    .description(EMPTY)
+                    .build());
+            existProjectByNameResponse(responseObserver, SUCCESS, SUCCESS.name(), false);
+        } catch (CoreException e) {
+            log.error("RpcService existProjectByName error CoreException ", e);
+            existProjectByNameResponse(responseObserver, SUCCESS, e.getMsg(), true);
+        } catch (Exception e) {
+            log.error("rpcService existProjectByName error Exception ", e);
+            existProjectByNameResponse(responseObserver, INTERNAL_ERROR, e.getMessage(), false);
+        }
+    }
 
     @Override
     public void containArchivedProjectsGet(
@@ -45,7 +169,7 @@ public class ProjectGrpcService extends ProjectServiceGrpc.ProjectServiceImplBas
                 throw CoreException.of(CoreException.ExceptionType.PARAMETER_INVALID);
             }
             TeamProto.GetTeamResponse response = teamGrpcClient.getTeam(request.getTeamId());
-            if (CodeProto.Code.SUCCESS != response.getCode()) {
+            if (SUCCESS != response.getCode()) {
                 builder.setCode(CodeProto.Code.NOT_FOUND);
                 builder.setMessage("Team is not found");
                 log.warn("Team is not found ,teamId : {}", request.getTeamId());
@@ -55,7 +179,7 @@ public class ProjectGrpcService extends ProjectServiceGrpc.ProjectServiceImplBas
                     builder.addAllProject(projects.stream()
                             .map(project -> toProtoProject(project, response.getData()))
                             .collect(Collectors.toList()));
-                    builder.setCode(CodeProto.Code.SUCCESS);
+                    builder.setCode(SUCCESS);
                 } else {
                     builder.setCode(CodeProto.Code.NOT_FOUND);
                     builder.setMessage("Project is not found");
@@ -64,11 +188,50 @@ public class ProjectGrpcService extends ProjectServiceGrpc.ProjectServiceImplBas
             }
         } catch (Exception e) {
             log.error("RpcService containArchivedProjectsGet error {} ", e.getMessage());
-            builder.setCode(CodeProto.Code.INTERNAL_ERROR)
+            builder.setCode(INTERNAL_ERROR)
                     .setMessage(e.getMessage());
         }
-
         responseObserver.onNext(builder.build());
+        responseObserver.onCompleted();
+    }
+
+    public void createProjectResponse(
+            StreamObserver<ProjectProto.CreateProjectResponse> responseObserver,
+            CodeProto.Code code,
+            String message,
+            Project project) {
+        ProjectProto.CreateProjectResponse.Builder builder = ProjectProto.CreateProjectResponse.newBuilder()
+                .setCode(code)
+                .setMessage(message);
+        if (Objects.nonNull(project)) {
+            TeamProto.GetTeamResponse response = teamGrpcClient.getTeam(project.getTeamOwnerId());
+            builder.setProject(toProtoProject(project, response.getData()));
+        }
+        responseObserver.onNext(builder.build());
+        responseObserver.onCompleted();
+    }
+
+    public void deleteProjectResponse(
+            StreamObserver<ProjectProto.DeleteProjectResponse> responseObserver,
+            CodeProto.Code code,
+            String message) {
+        responseObserver.onNext(ProjectProto.DeleteProjectResponse.newBuilder()
+                .setCode(code)
+                .setMessage(message)
+                .build());
+        responseObserver.onCompleted();
+    }
+
+    public void existProjectByNameResponse(
+            StreamObserver<ProjectProto.ExistProjectByNameResponse> responseObserver,
+            CodeProto.Code code,
+            String message,
+            Boolean isExist) {
+        responseObserver.onNext(ProjectProto.ExistProjectByNameResponse.newBuilder()
+                .setCode(code)
+                .setMessage(message)
+                .setIsExist(isExist)
+                .build());
         responseObserver.onCompleted();
     }
 
