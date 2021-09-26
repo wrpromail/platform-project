67ea30b0e534b0086ad506770e7175f98bc85fb8
liheng <liheng@coding.net>
2021-09-26 10:19:38 +0800

    星标项目迁移/ProjectSetting add compass

--- lib/src/main/java/net/coding/lib/project/service/ProjectPinService.java
+++ lib/src/main/java/net/coding/lib/project/service/ProjectPinService.java
@@ -1,22 +1,47 @@
 package net.coding.lib.project.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import net.coding.common.base.dao.BaseDao;
 import net.coding.common.util.BeanUtils;
+import net.coding.common.util.ResultPage;
+import net.coding.lib.project.dao.ProjectDao;
 import net.coding.lib.project.dao.ProjectPinDao;
+import net.coding.lib.project.dto.ProjectDTO;
+import net.coding.lib.project.entity.Project;
 import net.coding.lib.project.entity.ProjectPin;
+import net.coding.lib.project.exception.CoreException;
+import net.coding.lib.project.grpc.client.UserGrpcClient;
+import net.coding.lib.project.parameter.ProjectPageQueryParameter;
 
 import org.springframework.stereotype.Service;
 
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import proto.platform.user.UserProto;
+
+import static net.coding.lib.project.exception.CoreException.ExceptionType.RESOURCE_NO_FOUND;
+import static net.coding.lib.project.exception.CoreException.ExceptionType.USER_NOT_EXISTS;
 
 
 @Service
 @Slf4j
 @AllArgsConstructor
 public class ProjectPinService {
+    private final UserGrpcClient userGrpcClient;
     private final ProjectPinDao projectPinDao;
+    private final ProjectDTOService projectDTOService;
+    private final ProjectDao projectDao;
+    private final ProjectMemberService projectMemberService;
 
     public Optional<ProjectPin> getByProjectIdAndUserId(int projectId, int userId) {
         return Optional.ofNullable(projectPinDao.selectOne(ProjectPin.builder()
@@ -24,5 +49,91 @@ public class ProjectPinService {
                 .userId(userId)
                 .deletedAt(BeanUtils.getDefaultDeletedAt())
                 .build()));
+    }
+
+    /**
+     * 获取星标项目 支持关键字查看 支持分页
+     */
+    public ResultPage<ProjectDTO> getProjectPinPages(ProjectPageQueryParameter parameter) {
+        PageInfo<Project> pageInfo = PageHelper.startPage(parameter.getPage(), parameter.getPageSize())
+                .doSelectPageInfo(() -> projectPinDao.getProjectPinPages(parameter));
+        List<ProjectDTO> projectDTOList = pageInfo.getList().stream()
+                .map(projectDTOService::toDetailDTO)
+                .peek(p -> {
+                    p.setPin(getByProjectIdAndUserId(p.getId(), parameter.getUserId()).isPresent());
+                    p.setUn_read_activities_count(0);
+                })
+                .collect(Collectors.toList());
+        return new ResultPage<>(projectDTOList, parameter.getPage(), parameter.getPageSize(), pageInfo.getTotal());
+    }
+
+    public Boolean pinProject(Integer teamId, Integer userId, Integer projectId) throws CoreException {
+        Project project = checkProjectMember(teamId, userId, projectId);
+
+        if (getByProjectIdAndUserId(project.getId(), userId).isPresent()) {
+            return true;
+        }
+        return projectPinDao.insertSelective(
+                ProjectPin.builder()
+                        .projectId(project.getId())
+                        .userId(userId)
+                        .sort(projectPinDao.findMaxSort(userId) + 1)
+                        .createdAt(Timestamp.valueOf(LocalDateTime.now()))
+                        .deletedAt(Timestamp.valueOf(BaseDao.NOT_DELETED))
+                        .build()) > 0;
+    }
+
+    public Boolean cancelPinProject(Integer teamId, Integer userId, Integer projectId) throws CoreException {
+        Project project = checkProjectMember(teamId, userId, projectId);
+
+        getByProjectIdAndUserId(project.getId(), userId)
+                .ifPresent(pin -> {
+                    pin.setDeletedAt(new Timestamp(System.currentTimeMillis()));
+                    projectPinDao.updateByPrimaryKeySelective(pin);
+                });
+        return true;
+    }
+
+    public void sortPinProject(Integer teamId, Integer userId, Integer projectId, Integer targetId) throws CoreException {
+        Project sourceProject = checkProjectMember(teamId, userId, projectId);
+        ProjectPin sourcePin = getByProjectIdAndUserId(sourceProject.getId(), userId).orElse(null);
+        if (Objects.isNull(sourcePin)) {
+            throw CoreException.of(RESOURCE_NO_FOUND);
+        }
+        // 移到最下面
+        if (targetId == 0) {
+            sourcePin.setSort(projectPinDao.findMaxSort(userId) + 1);
+            projectPinDao.updateByPrimaryKeySelective(sourcePin);
+            return;
+        }
+        Project targetProject = checkProjectMember(teamId, userId, targetId);
+        ProjectPin targetPin = getByProjectIdAndUserId(targetProject.getId(), userId).orElse(null);
+        if (Objects.isNull(targetPin)) {
+            throw CoreException.of(RESOURCE_NO_FOUND);
+        }
+        Integer sourceSort = sourcePin.getSort();
+        Integer targetSort = targetPin.getSort();
+        if (sourceSort.equals(targetSort)) {
+            return;
+        }
+        projectPinDao.batchUpdateSortBlock(userId, sourceSort, targetSort);
+        // 将自己的sort更换成目标sort
+        sourcePin.setSort(sourceSort > targetSort ? targetSort : targetSort - 1);
+        projectPinDao.updateByPrimaryKeySelective(sourcePin);
+    }
+
+    public Project checkProjectMember(Integer teamId, Integer userId, Integer projectId) throws CoreException {
+        Project project = projectDao.getProjectByIdAndTeamId(projectId, teamId);
+        if (Objects.isNull(project)) {
+            throw CoreException.of(RESOURCE_NO_FOUND);
+        }
+        UserProto.User currentUser = userGrpcClient.getUserById(userId);
+        if (Objects.isNull(currentUser)) {
+            throw CoreException.of(USER_NOT_EXISTS);
+        }
+        if (!projectMemberService.isMember(currentUser, project.getId())) {
+            throw CoreException.of(CoreException.ExceptionType.PERMISSION_DENIED);
+        }
+        return project;
     }
 }
---
+++ server/src/main/java/net/coding/app/project/http/ProjectPinController.java
@@ -0,0 +1,90 @@
+package net.coding.app.project.http;
+
+import net.coding.app.project.constant.GatewayHeader;
+import net.coding.common.annotation.ProtectedAPI;
+import net.coding.common.util.Result;
+import net.coding.common.util.ResultPage;
+import net.coding.e.lib.core.bean.Project;
+import net.coding.lib.project.dto.ProjectDTO;
+import net.coding.lib.project.exception.CoreException;
+import net.coding.lib.project.form.QueryProgramForm;
+import net.coding.lib.project.form.QueryProjectForm;
+import net.coding.lib.project.parameter.ProjectPageQueryParameter;
+import net.coding.lib.project.service.ProjectPinService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@Api(value = "星标项目查询", tags = "星标项目查询")
+@AllArgsConstructor
+@RequestMapping("/api/platform/project")
+public class ProjectPinController {
+
+    private final ProjectPinService projectPinService;
+
+    @ProtectedAPI
+    @ApiOperation("星标项目-分页查询")
+    @PostMapping("/pin/pages")
+    public ResultPage<ProjectDTO> queryProjectPinPages(
+            @RequestHeader(GatewayHeader.TEAM_ID) Integer teamId,
+            @RequestHeader(GatewayHeader.USER_ID) Integer userId,
+            @RequestBody QueryProjectForm form) {
+        return projectPinService.getProjectPinPages(
+                ProjectPageQueryParameter.builder()
+                        .teamId(teamId)
+                        .userId(userId)
+                        .keyword(form.getKeyword())
+                        .page(form.getPage())
+                        .pageSize(form.getPageSize())
+                        .build());
+    }
+
+    @ProtectedAPI
+    @ApiOperation("星标项目-标记")
+    @PutMapping(value = {"/{projectId}/pin"})
+    public boolean pinProject(
+            @RequestHeader(GatewayHeader.TEAM_ID) Integer teamId,
+            @RequestHeader(GatewayHeader.USER_ID) Integer userId,
+            @PathVariable Integer projectId) throws CoreException {
+        return projectPinService.pinProject(teamId, userId, projectId);
+    }
+
+    @ProtectedAPI
+    @ApiOperation("星标项目-取消标记")
+    @DeleteMapping("/{projectId}/pin")
+    public boolean cancelPinProject(
+            @RequestHeader(GatewayHeader.TEAM_ID) Integer teamId,
+            @RequestHeader(GatewayHeader.USER_ID) Integer userId,
+            @PathVariable Integer projectId)
+            throws CoreException {
+        return projectPinService.cancelPinProject(teamId, userId, projectId);
+    }
+
+    @ProtectedAPI
+    @ApiOperation("星标项目-移动排序")
+    @PatchMapping("/{projectId}/pin/sort")
+    public Result sortPinProjects(
+            @RequestHeader(GatewayHeader.TEAM_ID) Integer teamId,
+            @RequestHeader(GatewayHeader.USER_ID) Integer userId,
+            @PathVariable Integer projectId,
+            @RequestParam(required = false, defaultValue = "0") Integer targetId
+    ) throws CoreException {
+        projectPinService.sortPinProject(teamId, userId, projectId, targetId);
+        return Result.success();
+    }
+}
