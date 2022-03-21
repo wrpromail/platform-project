5f01a4c687033635184db9dd521d79823fe99a10
刘扬洋 <liuyangyang@coding.net>
2022-03-21 15:03:24 +0800

    fixd: rediss password config

--- server/src/main/java/net/coding/app/project/config/RedissonConfig.java
+++ server/src/main/java/net/coding/app/project/config/RedissonConfig.java
@@ -1,8 +1,10 @@
 package net.coding.app.project.config;
 
+import org.apache.commons.lang3.StringUtils;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,12 +25,14 @@ public class RedissonConfig {
     public RedissonClient redissonClient() {
         Config config = new Config();
         String redisUrl = String.format("redis://%s:%s", host + "", port + "");
-        config.useSingleServer()
+        SingleServerConfig singleServerConfig = config.useSingleServer()
                 .setAddress(redisUrl)
-                .setPassword(password)
                 .setTimeout(3000)
                 .setConnectionMinimumIdleSize(2)
                 .setConnectionPoolSize(32);
+        if (StringUtils.isNotBlank(password)) {
+            singleServerConfig.setPassword(password);
+        }
         return Redisson.create(config);
     }
 }
