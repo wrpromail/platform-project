989e890b00ecafed3e8401d1f3eb7e8c5d040725
liheng <liheng@coding.net>
2022-05-21 16:31:08 +0800

    feat: OpenApi threadLocal 问题修复

--- lib/src/main/java/net/coding/lib/project/interceptor/GRpcHeaderServerInterceptor.java
+++ lib/src/main/java/net/coding/lib/project/interceptor/GRpcHeaderServerInterceptor.java
@@ -3,7 +3,7 @@ package net.coding.lib.project.interceptor;
 import net.coding.lib.project.common.DeployTokenHeader;
 import net.coding.lib.project.common.GRpcMetadataContextHolder;
 
-import io.grpc.ForwardingServerCall;
+import io.grpc.ForwardingServerCallListener;
 import io.grpc.Metadata;
 import io.grpc.ServerCall;
 import io.grpc.ServerCallHandler;
@@ -17,20 +17,76 @@ public class GRpcHeaderServerInterceptor implements ServerInterceptor {
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call,
                                                                  Metadata headers,
                                                                  ServerCallHandler<ReqT, RespT> next) {
-        log.info("header open-api client:" + headers);
-        String teamId = headers.get(Metadata.Key.of("TeamId", Metadata.ASCII_STRING_MARSHALLER));
-        String userId = headers.get(Metadata.Key.of("UserId", Metadata.ASCII_STRING_MARSHALLER));
-        String requestId = headers.get(Metadata.Key.of("RequestId", Metadata.ASCII_STRING_MARSHALLER));
-        String deployTokenId = headers.get(Metadata.Key.of("DeployTokenId", Metadata.ASCII_STRING_MARSHALLER));
-        String deployTokenProjectId = headers.get(Metadata.Key.of("DeployTokenProjectId", Metadata.ASCII_STRING_MARSHALLER));
-        GRpcMetadataContextHolder.set(DeployTokenHeader.builder()
-                .teamId(teamId)
-                .userId(userId)
-                .requestId(requestId)
-                .deployTokenId(deployTokenId)
-                .deployTokenProjectId(deployTokenProjectId)
-                .build());
-        return next.startCall(new ForwardingServerCall.SimpleForwardingServerCall<ReqT, RespT>(call) {
-        }, headers);
+        try {
+            log.info("header open-api client:" + headers);
+            String teamId = headers.get(Metadata.Key.of("TeamId", Metadata.ASCII_STRING_MARSHALLER));
+            String userId = headers.get(Metadata.Key.of("UserId", Metadata.ASCII_STRING_MARSHALLER));
+            String requestId = headers.get(Metadata.Key.of("RequestId", Metadata.ASCII_STRING_MARSHALLER));
+            String deployTokenId = headers.get(Metadata.Key.of("DeployTokenId", Metadata.ASCII_STRING_MARSHALLER));
+            String deployTokenProjectId = headers.get(Metadata.Key.of("DeployTokenProjectId", Metadata.ASCII_STRING_MARSHALLER));
+            DeployTokenHeader deployTokenHeader = DeployTokenHeader.builder()
+                    .teamId(teamId)
+                    .userId(userId)
+                    .requestId(requestId)
+                    .deployTokenId(deployTokenId)
+                    .deployTokenProjectId(deployTokenProjectId)
+                    .build();
+            GRpcMetadataContextHolder.set(deployTokenHeader);
+            final ServerCall.Listener<ReqT> listener = next.startCall(call, headers);
+            return new GRpcHeaderServerCallListener<>(listener, deployTokenHeader);
+        } finally {
+            GRpcMetadataContextHolder.remove();
+        }
+    }
+
+    public static class GRpcHeaderServerCallListener<ReqT> extends ForwardingServerCallListener<ReqT> {
+
+        private final ServerCall.Listener<ReqT> delegate;
+
+        private final DeployTokenHeader deployTokenHeader;
+
+        public GRpcHeaderServerCallListener(ServerCall.Listener<ReqT> delegate, DeployTokenHeader deployTokenHeader) {
+            this.delegate = delegate;
+            this.deployTokenHeader = deployTokenHeader;
+        }
+
+        @Override
+        protected ServerCall.Listener<ReqT> delegate() {
+            return delegate;
+        }
+
+        @Override
+        public void onMessage(ReqT message) {
+            wrapTracingRun(deployTokenHeader, () -> super.onMessage(message));
+        }
+
+        @Override
+        public void onHalfClose() {
+            wrapTracingRun(deployTokenHeader, super::onHalfClose);
+        }
+
+        @Override
+        public void onCancel() {
+            wrapTracingRun(deployTokenHeader, super::onCancel);
+        }
+
+        @Override
+        public void onComplete() {
+            wrapTracingRun(deployTokenHeader, super::onComplete);
+        }
+
+        @Override
+        public void onReady() {
+            wrapTracingRun(deployTokenHeader, super::onReady);
+        }
+    }
+
+    private static void wrapTracingRun(DeployTokenHeader deployTokenHeader, Runnable run) {
+        GRpcMetadataContextHolder.set(deployTokenHeader);
+        try {
+            run.run();
+        } finally {
+            GRpcMetadataContextHolder.remove();
+        }
     }
 }
